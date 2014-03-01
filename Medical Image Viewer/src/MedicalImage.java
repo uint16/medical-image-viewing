29032b856060da4a00e332adafaacbd1e169b206
Ethan <ethanmdavidson@gmail.com>
2014-03-01 16:27:40 -0500

    Finished up the MedicalImage and the Study

--- Medical Image Viewer/src/MedicalImage.java
+++ Medical Image Viewer/src/MedicalImage.java
@@ -1,29 +1,49 @@
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-
+/**
+ * Class representing a medical image in a study
+ * It proxies the actual image object so that it only gets loaded once
+ * @author Ethan Davidson (emd1771)
+ *
+ */
 public class MedicalImage{
 	private File imgFile;
 	private BufferedImage img;
+	private static BufferedImage errImg;
 
 	public MedicalImage(File f){
 		imgFile = f;
 		img = null;
+		
+		//errImg is static, so it is only loaded by the first image instantiated
+		if(errImg == null){
+			try{
+				errImg = ImageIO.read(new File("errorImage.jpg"));
+			} catch(IOException e){
+				System.err.println("Error image unable to be read!");
+			}
+		}
 	}
 	
+	/**
+	 * Loads the image (if it hasn't done so already) and returns it
+	 * If the image is unable to be loaded, it is pointed to the error image
+	 * 
+	 * @return BufferedImage the image represented by this proxy class
+	 */
 	public Image showImage(){
-		if(img != null){
-			return img;
-		} else {
+		if(img == null){
 			try{
 				img = ImageIO.read(imgFile);
-			} catch(Exception e){
-				img = errorImage;
+			} catch(IOException e){
+				img = errImg;
 			}
-			return img;
 		}
+		return img;
 	}
 }
--- Medical Image Viewer/src/Study.java
+++ Medical Image Viewer/src/Study.java
@@ -1,11 +1,53 @@
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 
+import org.apache.commons.io.FilenameUtils;
+
+enum ACCEPTABLE_FILE_EXT {JPG, JPEG};
 
 public class Study {
 	ArrayList<MedicalImage> images;
+	File folderPath;
 	
-	public Study(File folderPath){
-		
+	public Study(File f){
+		folderPath = f;
+		images = loadImages(folderPath);
+	}
+
+	/**
+	 * Iterates over the files in given folder and creates a MedicalImage for each file matching the acceptable extensions
+	 * 
+	 * @param folder File object pointing to the folder containing the images to load
+	 * @return ArrayList of MedicalImages
+	 */
+	private ArrayList<MedicalImage> loadImages(File folder) {
+		ArrayList<MedicalImage> result = new ArrayList<MedicalImage>();
+		for(File f: folder.listFiles(validExtensionFilter())){
+			result.add(new MedicalImage(f));
+		}
+		return result;
+	}
+	
+	/**
+	 * returns a FilenameFilter which only accepts files of the extension types listed in the enum ACCEPTABLE_FILE_EXT
+	 * This should probably be generated once and then passed by reference
+	 * 
+	 * @return FilenameFilter accepting enumerated file extensions
+	 */
+	private FilenameFilter validExtensionFilter(){
+		FilenameFilter result = new FilenameFilter(){
+			@Override
+			public boolean accept(File dir, String name){
+				String ext = FilenameUtils.getExtension(name);
+				for(ACCEPTABLE_FILE_EXT a : ACCEPTABLE_FILE_EXT.values()){
+					if(a.name().equalsIgnoreCase(ext)){
+						return true;
+					}
+				}
+				return false;
+			}
+		};
+		return result;
 	}
 }
