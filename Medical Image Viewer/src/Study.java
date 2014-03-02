da55ca4563de9dae1ecf1525ace62e000117dcad
Ethan <ethanmdavidson@gmail.com>
2014-03-02 15:06:36 -0500

    Tweaked the Study (the FilenameFilter is now static)

--- Medical Image Viewer/src/Study.java
+++ Medical Image Viewer/src/Study.java
@@ -4,13 +4,34 @@ import java.util.ArrayList;
 
 import org.apache.commons.io.FilenameUtils;
 
-enum ACCEPTABLE_FILE_EXT {JPG, JPEG};
+enum ACCEPTABLE_FILE_EXT {
+	JPG, JPEG
+};
 
+/**
+ * Class representing a study (collection of medical images)
+ * Holds a collection of images and the File representing the folder the study is in
+ * 
+ * @author Ethan Davidson (emd1771)
+ *
+ */
 public class Study {
 	ArrayList<MedicalImage> images;
 	File folderPath;
-	
-	public Study(File f){
+	private static FilenameFilter validExtFilter = new FilenameFilter() {
+		@Override
+		public boolean accept(File dir, String name) {
+			String ext = FilenameUtils.getExtension(name);
+			for (ACCEPTABLE_FILE_EXT a : ACCEPTABLE_FILE_EXT.values()) {
+				if (a.name().equalsIgnoreCase(ext)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	};
+
+	public Study(File f) {
 		folderPath = f;
 		images = loadImages(folderPath);
 	}
@@ -18,36 +39,15 @@ public class Study {
 	/**
 	 * Iterates over the files in given folder and creates a MedicalImage for each file matching the acceptable extensions
 	 * 
-	 * @param folder File object pointing to the folder containing the images to load
+	 * @param folder
+	 *            File object pointing to the folder containing the images to load
 	 * @return ArrayList of MedicalImages
 	 */
 	private ArrayList<MedicalImage> loadImages(File folder) {
 		ArrayList<MedicalImage> result = new ArrayList<MedicalImage>();
-		for(File f: folder.listFiles(validExtensionFilter())){
+		for (File f : folder.listFiles(validExtFilter)) {
 			result.add(new MedicalImage(f));
 		}
-		return result;
-	}
-	
-	/**
-	 * returns a FilenameFilter which only accepts files of the extension types listed in the enum ACCEPTABLE_FILE_EXT
-	 * This should probably be generated once and then passed by reference
-	 * 
-	 * @return FilenameFilter accepting enumerated file extensions
-	 */
-	private FilenameFilter validExtensionFilter(){
-		FilenameFilter result = new FilenameFilter(){
-			@Override
-			public boolean accept(File dir, String name){
-				String ext = FilenameUtils.getExtension(name);
-				for(ACCEPTABLE_FILE_EXT a : ACCEPTABLE_FILE_EXT.values()){
-					if(a.name().equalsIgnoreCase(ext)){
-						return true;
-					}
-				}
-				return false;
-			}
-		};
 		return result;
 	}
 }
