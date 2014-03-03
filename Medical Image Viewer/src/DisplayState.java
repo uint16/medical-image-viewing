e03e99d0d9627fe8920b88f2e4667084499b3c3d
Ethan <ethanmdavidson@gmail.com>
2014-03-02 23:02:23 -0500

    Display state now saves and loads (very basic). Also some minor refactoring to the FourUp

--- Medical Image Viewer/src/DisplayState.java
+++ Medical Image Viewer/src/DisplayState.java
@@ -1,25 +1,74 @@
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.util.List;
 import java.util.Observable;
 
 import javax.swing.JFrame;
 
+import org.apache.commons.io.FileUtils;
 
-public class DisplayState extends Observable{
+public class DisplayState extends Observable {
 	int index;
 	DisplayMode mode;
 	Study study;
-	
-	public DisplayState(Study s){
+
+	public DisplayState(Study s) {
 		index = 0;
 		mode = new OneUp();
 		study = s;
 	}
-	
-	public JFrame generateFrame(){
+
+	/**
+	 * Generates a JFrame containing the images currently being viewed
+	 * @return JFrame containing the images currently being viewed
+	 */
+	public JFrame generateFrame() {
 		JFrame result = new JFrame();
 		result.setLayout(mode.getLayout());
-		for(int i : mode.getIndices(index)){
+		for (int i : mode.getIndices(index)) {
 			result.add(new ImagePanel(study.getImage(i)));
 		}
 		return result;
+	}
+
+	/**
+	 * saves the display state to a text file in the study folder
+	 * TODO: use something more robust than a text file (possibly preferences?)
+	 */
+	public void save() {
+		File saveFile = new File(study.folderPath, "displayState.txt");
+		try {
+			FileUtils.writeStringToFile(saveFile, Integer.toString(index) + "\n");
+			FileUtils.writeStringToFile(saveFile, mode.getClass().getName() + "\n");
+		} catch (IOException e) {
+			System.err.println("Error saving display state: " + saveFile.toString());
+		}
+	}
+	
+	/**
+	 * loads the display state from the text file in the study folder
+	 * TODO: use something more robust than a text file
+	 */
+	public void load(){
+		File saveFile = new File(study.folderPath, "displayState.txt");
+		
+		if (saveFile.exists()) {
+			try {
+				List<String> data = FileUtils.readLines(saveFile);
+				this.index = Integer.parseInt(data.get(0));
+				//dynamically load the right DisplayMode
+				try {
+					Class<?> m = Class.forName(data.get(1));
+					Constructor<?> c = m.getConstructor();
+					mode = (DisplayMode) c.newInstance();
+				} catch (Exception e) {	
+					e.printStackTrace();
+					mode = new OneUp();
+				}
+			} catch (IOException e) {
+				System.err.println("Error loading display state: " + saveFile.toString());
+			}
+		}
 	}
 }
--- Medical Image Viewer/src/FourUp.java
+++ Medical Image Viewer/src/FourUp.java
@@ -3,11 +3,12 @@ import java.awt.LayoutManager;
 
 
 public class FourUp implements DisplayMode {
+	private static int IMG_PER_PAGE = 4;
 
 	@Override
 	public int nextIndex(int index, Study s) {
-		if(index+4 < s.imgAmt()){
-			return index + 4;
+		if(index+IMG_PER_PAGE <= s.imgAmt()){
+			return index + IMG_PER_PAGE;
 		} else {
 			return index;
 		}
@@ -15,8 +16,8 @@ public class FourUp implements DisplayMode {
 
 	@Override
 	public int prevIndex(int index, Study s) {
-		if(index-4 > 0){
-			return index - 4;
+		if(index-IMG_PER_PAGE >= 0){
+			return index - IMG_PER_PAGE;
 		} else {
 			return index;
 		}
@@ -29,11 +30,10 @@ public class FourUp implements DisplayMode {
 
 	@Override
 	public int[] getIndices(int index) {
-		int[] result = new int[4];
-		result[0] = 4*((int) Math.floor(index/4))+0;
-		result[1] = 4*((int) Math.floor(index/4))+1;
-		result[2] = 4*((int) Math.floor(index/4))+2;
-		result[3] = 4*((int) Math.floor(index/4))+3;
+		int[] result = new int[IMG_PER_PAGE];
+		for(int i = 0; i < result.length; i++){
+			result[i] = IMG_PER_PAGE*((int) Math.floor(index/IMG_PER_PAGE))+i;
+		}
 		return result;
 	}
 
